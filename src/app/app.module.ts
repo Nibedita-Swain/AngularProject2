8eeb556c9c8a047df7f19e798147c44f2c80ae9d
Nibedita Swain <90179743+Nibedita-Swain@users.noreply.github.com>
2021-10-03 21:00:34 +0530

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,11 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { PriceComponent } from './price/price.component';
 import { FormsModule } from '@angular/forms';
 import { TodosComponent } from './todos/todos.component';
 @NgModule({
   declarations: [
     AppComponent,
-    PriceComponent,
     TodosComponent
   ],
   imports: [
